0f1c3e90694fdc63629a36073bf00f90b7539d25
Joseph A. Oswald, III <josephoswald@gmail.com>
2026-01-06 22:44:00 -0500

    test 4-byte routines

--- endian_test.cc
+++ endian_test.cc
@@ -12,3 +12,15 @@ TEST(Endian, TwoBytes) {
   uint16_t read_val = BigEndian2(b);
   EXPECT_EQ(val, read_val);
 }
+
+TEST(Endian, FourBytes) {
+  char b[4];
+  const uint32_t val = 0x12345678;
+  WriteBigEndian4(val, b);
+  EXPECT_EQ(b[0], 0x12);
+  EXPECT_EQ(b[1], 0x34);
+  EXPECT_EQ(b[2], 0x56);
+  EXPECT_EQ(b[3], 0x78);
+  const uint32_t read_val = BigEndian4(b);
+  EXPECT_EQ(val, read_val);
+}
